ba20f86c96dfece23251ed01545cd9b318e8f2f3
Murari <perumallamurari@gmail.com>
2026-02-02 08:32:22 +0530

    Added: UC7: Stored Character Banner Patterns Separately

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -5,14 +5,39 @@ public class OOPSBannerApp {
     }
 
     static void printBanner() {
-        for (String line : new String[]{
-                " OOO   OOO  PPPP   SSS ",
-                "O   O O   O P   P S    ",
-                "O   O O   O PPPP   SSS ",
-                "O   O O   O P         ",
-                " OOO   OOO  P      SSS"
-        }) {
-            System.out.println(line);
+
+        String[] O = {
+                " OOO ",
+                "O   O",
+                "O   O",
+                "O   O",
+                " OOO "
+        };
+
+        String[] P = {
+                "PPPP ",
+                "P   P",
+                "PPPP ",
+                "P    ",
+                "P    "
+        };
+
+        String[] S = {
+                " SSS ",
+                "S    ",
+                " SSS ",
+                "    S",
+                " SSS "
+        };
+
+        // Rendering OOPS manually using character arrays
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(
+                    O[i] + " " +
+                            O[i] + " " +
+                            P[i] + " " +
+                            S[i]
+            );
         }
     }
 }
